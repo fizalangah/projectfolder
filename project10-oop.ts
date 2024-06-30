1422104e5de900873db332f4a6325fdef2204c2e
fiza <fizalangah2001@gmail.com>
2024-06-30 14:44:11 +0500

    oop project

---
+++ project10-oop.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+class Student {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Person {
+  students: Student[] = [];
+  addstudent(obj: Student) {
+    this.students.push(obj);
+  }
+}
+const persons = new Person();
+
+const programstsrt = async (persons: Person) => {
+  do {
+    console.log("!Welcom");
+
+    const ans = await inquirer.prompt({
+      name: "select",
+      type: "list",
+      message: "whome would you like to interreact",
+      choices: ["staff", "student", "exit"],
+    });
+    if (ans.select === "staff") {
+      console.log("you approach the staff room ,feel fre to ask any question");
+    } else if (ans.select === "student") {
+      const sudentinfo = await inquirer.prompt({
+        name: "student",
+        type: "input",
+        message: "enter the student name with you want to interact",
+      });
+      const student = persons.students.find(
+        (val) => val.name === sudentinfo.student
+      );
+
+      if (!student) {
+        const name = new Student(sudentinfo.student);
+        persons.addstudent(name);
+        console.log(`Hello ${name.name} nice to meet you`);
+        console.log("new student added");
+        console.log("current student list");
+        console.log(persons.students);
+      } else {
+        console.log(`hello i am ${student.name} nice to see you again`);
+        console.log("Existing student list");
+        console.log(persons.students);
+      }
+    } else if (ans.select === "exit") {
+      console.log("exiting the program");
+      process.exit();
+    }
+  } while (true);
+};
+
+programstsrt(persons);
